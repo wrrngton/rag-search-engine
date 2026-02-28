9751e63e5ac076b64bd364ca5b673095cf5bb0d6
wrrngton <hello@jezl.xyz>
2026-02-28 14:10:40 +0000

    first commit

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(
+        dest="command", help="Available commands")
+    search_parser = subparsers.add_parser(
+        "search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            print(f"Searching {args.query}")
+        case _:
+            parser.print_help()
+
+if __name__ == "__main__":
+    main()
